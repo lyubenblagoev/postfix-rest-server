fb8707a984978e3cd55870c57bfd38fe39dc1f86
Lyuben Blagoev <lblagoev@gmail.com>
2021-02-19 13:20:13 +0200

    Enable cross origin requests to the API

---
+++ src/main/java/com/lyubenblagoev/postfixrest/configuration/CorsConfiguration.java
@@ -0,0 +1,20 @@
+package com.lyubenblagoev.postfixrest.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration {
+
+    @Bean
+    WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/v1/**");
+            }
+        };
+    }
+}
