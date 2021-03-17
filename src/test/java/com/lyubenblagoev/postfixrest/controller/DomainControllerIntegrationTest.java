25e9a989288e887b471825eb37acde1e78b7f3e1
Lyuben Blagoev <lblagoev@gmail.com>
2021-03-17 16:20:28 +0200

    Fix domain integration tests after adding authentication

--- src/test/java/com/lyubenblagoev/postfixrest/controller/DomainControllerIntegrationTest.java
+++ src/test/java/com/lyubenblagoev/postfixrest/controller/DomainControllerIntegrationTest.java
@@ -1,5 +1,6 @@
 package com.lyubenblagoev.postfixrest.controller;
 
+import com.lyubenblagoev.postfixrest.security.JwtTokenProvider;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
@@ -10,6 +11,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Collections;
 
 import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -23,30 +26,41 @@ class DomainControllerIntegrationTest {
 
     public static final String PATH_API_V_1_DOMAINS = "/api/v1/domains";
 
+    private final MockMvc mockMvc;
+    private final JwtTokenProvider jwtTokenProvider;
+
     @Autowired
-    private MockMvc mockMvc;
+    public DomainControllerIntegrationTest(MockMvc mockMvc, JwtTokenProvider jwtTokenProvider) {
+        this.mockMvc = mockMvc;
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
 
     @Test
     @Order(1)
     public void getAllDomains_shouldReturnEmptyList() throws Exception {
-        mockMvc.perform(get(PATH_API_V_1_DOMAINS))
+        String token = jwtTokenProvider.createToken("admin@example.com", Collections.emptyList());
+        mockMvc.perform(
+                get(PATH_API_V_1_DOMAINS)
+                        .header("Authorization", "Bearer " + token))
                 .andExpect(status().isOk())
                 .andExpect(content().string(containsString("[]")));
     }
 
     @Test
     @Order(2)
-    public void getDomain_shouldReturn404() throws Exception {
+    public void getDomain_shouldReturn403() throws Exception {
         mockMvc.perform(get(PATH_API_V_1_DOMAINS + "/example.com"))
-                .andExpect(status().is4xxClientError());
+                .andExpect(status().isForbidden());
     }
 
     @Test
     @Order(3)
     public void addDomain_shouldSucceed() throws Exception {
+        String token = jwtTokenProvider.createToken("admin@example.com", Collections.emptyList());
         mockMvc.perform(
                 post(PATH_API_V_1_DOMAINS)
                         .contentType(MediaType.APPLICATION_JSON)
+                        .header("Authorization", "Bearer " + token)
                         .content("{ \"name\": \"example.com\", \"enabled\": true }"))
                 .andExpect(status().is2xxSuccessful())
                 .andExpect(jsonPath("$.name", is("example.com")))
@@ -57,9 +71,11 @@ class DomainControllerIntegrationTest {
     @Test
     @Order(4)
     public void edit_shouldDisableDomain() throws Exception {
+        String token = jwtTokenProvider.createToken("admin@example.com", Collections.emptyList());
         mockMvc.perform(
                 put(PATH_API_V_1_DOMAINS + "/example.com")
                         .contentType(MediaType.APPLICATION_JSON)
+                        .header("Authorization", "Bearer " + token)
                         .content("{ \"name\": \"example.com\", \"enabled\": false }"))
                 .andExpect(status().is2xxSuccessful())
                 .andExpect(jsonPath("$.name", is("example.com")))
@@ -69,7 +85,10 @@ class DomainControllerIntegrationTest {
     @Test
     @Order(5)
     public void getAllDomains_shouldReturnListOfOne() throws Exception {
-        mockMvc.perform(get(PATH_API_V_1_DOMAINS))
+        String token = jwtTokenProvider.createToken("admin@example.com", Collections.emptyList());
+        mockMvc.perform(
+                get(PATH_API_V_1_DOMAINS)
+                        .header("Authorization", "Bearer " + token))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(1)));
     }
@@ -77,7 +96,10 @@ class DomainControllerIntegrationTest {
     @Test
     @Order(6)
     public void getDomain_shouldReturnDisabledDomainInstance() throws Exception {
-        mockMvc.perform(get(PATH_API_V_1_DOMAINS + "/example.com"))
+        String token = jwtTokenProvider.createToken("admin@example.com", Collections.emptyList());
+        mockMvc.perform(
+                get(PATH_API_V_1_DOMAINS + "/example.com")
+                        .header("Authorization", "Bearer " + token))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.name", is("example.com")))
                 .andExpect(jsonPath("$.enabled", is(false)));
@@ -86,14 +108,20 @@ class DomainControllerIntegrationTest {
     @Test
     @Order(7)
     public void delete_shouldSucceed() throws Exception {
-        mockMvc.perform(delete(PATH_API_V_1_DOMAINS + "/example.com"))
+        String token = jwtTokenProvider.createToken("admin@example.com", Collections.emptyList());
+        mockMvc.perform(
+                delete(PATH_API_V_1_DOMAINS + "/example.com")
+                        .header("Authorization", "Bearer " + token))
                 .andExpect(status().isOk());
     }
 
     @Test
     public void delete_nonexistentShouldFail() throws Exception {
-        mockMvc.perform(delete(PATH_API_V_1_DOMAINS + "/example.org"))
-                .andExpect(status().is4xxClientError());
+        String token = jwtTokenProvider.createToken("admin@example.com", Collections.emptyList());
+        mockMvc.perform(
+                delete(PATH_API_V_1_DOMAINS + "/example.org")
+                        .header("Authorization", "Bearer " + token))
+                .andExpect(status().isNotFound());
     }
 
 }
