fa33bbf2bdfd241b7af0afb1663bea6fbc27334d
Lyuben Blagoev <lblagoev@gmail.com>
2021-03-20 10:21:39 +0200

    Make domain - alias/account mapping bidirectional cascade remove operations

--- src/main/java/com/lyubenblagoev/postfixrest/entity/Domain.java
+++ src/main/java/com/lyubenblagoev/postfixrest/entity/Domain.java
@@ -1,13 +1,21 @@
 package com.lyubenblagoev.postfixrest.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.Collections;
+import java.util.List;
 
 @Entity
 @Table(name="domains")
 public class Domain extends BaseEntity {
-	
+
+	@Column(nullable = false)
 	private String name;
+
+	@OneToMany(orphanRemoval = true, cascade = CascadeType.ALL, mappedBy = "domain")
+	private List<Account> accounts = Collections.emptyList();
+
+	@OneToMany(orphanRemoval = true, cascade = CascadeType.ALL, mappedBy = "domain")
+	private List<Alias> aliases = Collections.emptyList();
 
 	public String getName() {
 		return name;
@@ -17,4 +25,19 @@ public class Domain extends BaseEntity {
 		this.name = name;
 	}
 
+	public List<Account> getAccounts() {
+		return accounts;
+	}
+
+	public void setAccounts(List<Account> accounts) {
+		this.accounts = accounts;
+	}
+
+	public List<Alias> getAliases() {
+		return aliases;
+	}
+
+	public void setAliases(List<Alias> aliases) {
+		this.aliases = aliases;
+	}
 }
